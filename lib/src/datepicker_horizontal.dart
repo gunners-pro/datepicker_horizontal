f07d96962e798756cdb22aedc6c56cf2f40c2c35
Fabricyo <bass.barreto@gmail.com>
2026-04-12 16:37:51 -0300

    docs: add dart doc

--- lib/src/datepicker_horizontal.dart
+++ lib/src/datepicker_horizontal.dart
@@ -2,18 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'widgets/date_item.dart';
 
+/// A customizable horizontal date picker widget.
+///
+/// Displays a scrollable list of dates starting from [initialDate],
+/// allowing the user to select a date.
+///
+/// The selected date is returned via [onDateSelected].
+///
+/// Supports localization using the `intl` package.
+///
+/// Example:
+/// ```dart
+/// DatePickerHorizontal(
+///   initialDate: DateTime.now(),
+///   daysCount: 7,
+///   locale: 'pt_BR',
+///   onDateSelected: (date) {
+///     print(date);
+///   },
+/// )
+/// ```
 class DatePickerHorizontal extends StatefulWidget {
+  /// The initial date displayed in the picker.
   final DateTime initialDate;
+
+  /// Number of days generated starting from [initialDate].
   final int daysCount;
+
+  /// Callback triggered when a date is selected.
   final ValueChanged<DateTime>? onDateSelected;
+
+  /// Locale used to format weekday names (e.g. 'en_US', 'pt_BR').
   final String? locale;
 
+  /// Background color of the selected date item.
   final Color selectedColor;
+
+  /// Default text color for unselected items.
   final Color textColor;
+
+  /// Text color for the weekday when selected.
   final Color selectedWeekDayTextColor;
+
+  /// Text color for the day number when selected.
   final Color selectedDayTextColor;
 
+  /// Width of each date item.
   final double itemWidth;
+
+  /// Height of the date picker.
   final double itemHeight;
 
   const DatePickerHorizontal({
@@ -45,14 +82,17 @@ class _DatePickerHorizontalState extends State<DatePickerHorizontal> {
         (i) => widget.initialDate.add(Duration(days: i)),
       );
 
+  /// Handles tap on a date item.
   void _onTap(DateTime date) {
     setState(() => _selectedDate = date);
     widget.onDateSelected?.call(date);
   }
 
+  /// Returns true if both dates represent the same calendar day.
   bool _isSameDay(DateTime a, DateTime b) =>
       a.year == b.year && a.month == b.month && a.day == b.day;
 
+  /// Formats the weekday label based on the provided [locale].
   String _formatWeekday(DateTime date) =>
       DateFormat.E(widget.locale).format(date);
 
